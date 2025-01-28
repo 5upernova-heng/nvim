aea4b03b2bf73132e19a50ab4626ce76309a8669
Heng Yu <h3ngyu@gmail.com>
2025-01-29 00:47:25 +0800

    Feat(lsp): frontend support(typescript & json)

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -180,6 +180,8 @@ return {
         clangd = {},
         pyright = {},
         rust_analyzer = {},
+        ts_ls = {},
+        jsonls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
