c6e5cf6ee7d80931dbb1ff270d20d5859e3b921d
Heng Yu <h3ngyu@gmail.com>
2025-10-02 16:07:05 +0800

    Fix: nvim-treesitter is loaded before markview

--- lua/custom/plugins/markview.lua
+++ lua/custom/plugins/markview.lua
@@ -145,11 +145,6 @@ return {
     }
   end,
   dependencies = {
-    -- You will not need this if you installed the
-    -- parsers manually
-    -- Or if the parsers are in your $RUNTIMEPATH
-    'nvim-treesitter/nvim-treesitter',
-
     'nvim-tree/nvim-web-devicons',
   },
 }
